57c2c5a20389a7775adaa735e1672020c0b8d74d
obenjic1 <58641873+obenjic1@users.noreply.github.com>
2019-12-07 23:22:04 +0100

    Add files via upload

---
+++ TestMethods.java
@@ -0,0 +1,16 @@
+public class TestMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 20;
+int y =10;
+
+Display object = new Display();
+object.x=x;
+object.y=y;
+object.DisplayIt();
+object.displayItTimesTwo();
+object.displayItPlusOneHundred();
+	}
+
+}
